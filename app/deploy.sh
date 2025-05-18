cb889212619a994b937c65b152f7357a139a6853
Kyle Ton <kht@Kyles-MacBook-Air-M3.local>
2025-05-18 11:56:26 -0700

    using aws ec2, set up docker container files

---
+++ app/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Pulling changes"
+git pull origin main
+
+echo "Rebuilding Docker image"
+docker compose build
+
+echo "Restarting FastAPI container"
+docker compose down
+docker compose up -d
+
+echo "✅ Deployed!"
