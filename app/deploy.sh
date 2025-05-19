739579083538fb89d5a47fba2c3bd9aa7fbad47d
Kyle Ton <kht@Kyles-MacBook-Air-M3.local>
2025-05-18 18:35:48 -0700

    changed commands in deploy shell

--- app/deploy.sh
+++ app/deploy.sh
@@ -7,7 +7,7 @@ echo "Rebuilding Docker image"
 docker compose build
 
 echo "Restarting FastAPI container"
-docker compose down
-docker compose up -d
+docker-compose down
+docker-compose up -d --build
 
 echo "✅ Deployed!"
